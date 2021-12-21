178b9fa1f187d13ff1272654ebc8af0ac85f2569
Flávio Arêas <vflavio9@gmail.com>
2021-12-21 18:12:27 -0300

    feat: data format

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,25 +1,22 @@
 import { useMemo } from 'react';
 import { ProductItem } from './ProductItem';
 
-interface SSearchResultsProps {
+interface SearchResultsProps {
+  totalPrice: number;
   results: Array<{
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   }>;
   onAddToWishlist: (id: number) => void;
 }
 
 export function SearchResults({
+  totalPrice,
   results,
   onAddToWishlist,
-}: SSearchResultsProps) {
-  const totalPrice = useMemo(() => {
-    return results.reduce((total, product) => {
-      return total + product.price;
-    }, 0);
-  }, [results]);
-
+}: SearchResultsProps) {
   return (
     <div>
       <h2>{totalPrice}</h2>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import { FormEvent, useCallback, useState } from 'react';
 import { SearchResults } from '../components/SearchResults';
 
+interface Results {
+  totalPrice: number;
+  data: Array<{
+    id: number;
+    price: number;
+    priceFormatted: string;
+    title: string;
+  }>;
+}
+
 export default function Home() {
   const [search, setSearch] = useState('');
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Results>({
+    data: [],
+    totalPrice: 0,
+  });
 
   async function handleSearch(e: FormEvent) {
     e.preventDefault();
@@ -15,7 +28,23 @@ export default function Home() {
     const response = await fetch(`http://localhost:3333/products?q=${search}`);
     const data = await response.json();
 
-    setResults(data);
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    const products = data.map((product) => ({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      priceFormatted: formatter.format(product.price),
+    }));
+
+    const totalPrice = data.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+
+    setResults({ data: products, totalPrice });
   }
 
   const addToWishlist = useCallback(async (id: number) => {
@@ -36,7 +65,11 @@ export default function Home() {
         <button type="submit">Buscar</button>
       </form>
 
-      <SearchResults results={results} onAddToWishlist={addToWishlist} />
+      <SearchResults
+        results={results.data}
+        totalPrice={results.totalPrice}
+        onAddToWishlist={addToWishlist}
+      />
     </div>
   );
 }
