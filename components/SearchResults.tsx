5c9f71600f2e88350b8db893a0f5dc1cc3cec62e
Flávio Arêas <vflavio9@gmail.com>
2021-12-21 17:38:54 -0300

    feat: useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { ProductItem } from './ProductItem';
 
 interface SSearchResultsProps {
@@ -9,8 +10,16 @@ interface SSearchResultsProps {
 }
 
 export function SearchResults({ results }: SSearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map((result) => (
         <ProductItem key={result.id} product={result} />
       ))}
